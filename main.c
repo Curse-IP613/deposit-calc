b638a881744488f3fc544d53a6494d80d9606e53
Сёмин Артём Васильевич <ip613s20@mwp03.csc.local>
2017-03-02 08:22:18 +0700

    edit main.c and creating main.exe v2

--- main.c
+++ main.c
@@ -9,13 +9,13 @@ int main()
     scanf("%i",&t);
     printf("enter sum to deposit: \n");
     scanf("%i",&s);
-    if ((t <= 0) || (t > 365) || (s<10000))
+          if ((t <= 0) || (t > 365) || (s<10000))
     printf("enter correct data! \n");
-    else
-	{
-		printf("sum at the end of: ");
-        printf("%i",proffit(s,t));
-    }
+          else
+	  {
+	 	printf("sum at the end of: ");
+        printf("%.0f",proffit(s,t));
+          }
     return 0;
 }
 float proffit(int x,int z){
@@ -24,22 +24,22 @@ float proffit(int x,int z){
 	{
 	    if ((z > 0) && (z <= 30)) 
         cashback*=0.9;
-        if ((z > 30) && (z <= 120)) 
+            if ((z > 30) && (z <= 120)) 
         cashback*=1.02;
-        if ((z > 120) && (z <= 240)) 
+            if ((z > 120) && (z <= 240)) 
         cashback*=1.06;
-         if ((z > 240) && (z <= 365)) 
+            if ((z > 240) && (z <= 365)) 
         cashback*=1.12;
     }
     	else
 	{
 		if ((z > 0) && (z <= 30)) 
         cashback*=0.9;
-        if ((z > 30) && (z <= 120)) 
+                if ((z > 30) && (z <= 120)) 
         cashback*=1.03;
-        if ((z > 120) && (z <= 240)) 
+                if ((z > 120) && (z <= 240)) 
         cashback*=1.08;
-         if ((z > 240) && (z <= 365)) 
+                if ((z > 240) && (z <= 365)) 
         cashback*=1.15;
 	}
 return cashback;
